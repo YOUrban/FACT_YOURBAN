88b6775dc22cd2b3fb58b4ad5e9888988dac2477
Max Barbet <max.barbet@go-yourban.com>
2024-12-06 11:51:51 +0100

    updates

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # Fichier: main.py
 import streamlit as st
-from streamlit_searchbox import st_searchbox
-import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
